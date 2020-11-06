1dd3715a55ec82a931844f5e31d3ac0ffe65a0fa
maumainsaw <maumainsaw@gmail.com>
2020-11-05 22:59:10 -0600

    Creando las 3 secciones

--- src/components/InfoSection/Data.jsx
+++ src/components/InfoSection/Data.jsx
@@ -1,5 +1,5 @@
 export const homeObjOne = {
-    id:'about',
+    id:'discover',
     lightBg:true,
     lightText:false,
     lightTextDesc:false,
@@ -13,5 +13,37 @@ export const homeObjOne = {
     dark:false,
     primary:false,
     darkText:true
+}
+export const homeObjTwo = {
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headline:'Unlimeted Transactions with zero fees',
+    description:'Get acess to pur exclusive app that allows you to send unlimited transcactions',
+    buttonLabel:'Get started',
+    imgStart:true,
+    img:'./images/2.svg',
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+}
 
+export const homeObjThree = {
+    id:'signup',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Premium Bank',
+    headline:'Unlimeted Transactions with zero fees',
+    description:'Get acess to pur exclusive app that allows you to send unlimited transcactions',
+    buttonLabel:'Get started',
+    imgStart:false,
+    img:'./images/3.svg',
+    alt:'Car',
+    dark:false,
+    primary:false,
+    darkText:true
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import HeroSection from '../components/HeroSection/HeroSection';
-import { homeObjOne } from '../components/InfoSection/Data';
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
 import InfoSection from '../components/InfoSection/InfoSection';
 import Navbar from '../components/Navbar/Navbar'
 import Sidebar from '../components/Sidebar/Sidebar'
@@ -18,6 +18,8 @@ const Homepage = () => {
         <Navbar toggle={toggle}/>
         <HeroSection/>
         <InfoSection {...homeObjOne}/>
+        <InfoSection {...homeObjTwo}/>
+        <InfoSection {...homeObjThree}/>
         </>
         
     )
